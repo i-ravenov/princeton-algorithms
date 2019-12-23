c6e54a1f79f2e0485ccbf27bec7ea41b002be420
ivoronov <ioann.voronov@gmail.com>
2019-12-24 00:29:24 +0200

    Initial commit for KdTree programming assignment

---
+++ assignments/kdtree/KdTree.java
@@ -0,0 +1,43 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+import java.util.ArrayList;
+
+public class KdTree {
+
+    public KdTree() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void insert(Point2D p) {
+
+    }
+
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    public void draw() {
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        return new ArrayList<>();
+    }
+
+    public Point2D nearest(Point2D p) {
+        return new Point2D(0, 0);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
